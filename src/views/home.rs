485cd208a42307d5112660e67b1681daa79c667c
Lukas Turcani <lukasturcani93@gmail.com>
2024-12-26 12:31:18 +0100

    Add tailwind deps

--- src/views/home.rs
+++ src/views/home.rs
@@ -5,6 +5,10 @@ use dioxus::prelude::*;
 pub fn Home() -> Element {
     rsx! {
         Hero {}
+        div {
+            class: "bg-red-500 p-4 text-center dark:bg-green-500",
+            "Hello, world!"
+        }
 
     }
 }
