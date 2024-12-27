7bb0eeceacdc4beac3d0f9e031d5c4288e1c41db
Lukas Turcani <lukasturcani93@gmail.com>
2024-12-27 10:25:44 +0100

    nice

--- backend/src/millie/main.py
+++ backend/src/millie/main.py
@@ -1,5 +1,8 @@
 """The backend."""
 
+from pathlib import Path
+from typing import Literal
+
 from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 
@@ -9,8 +12,9 @@ app = FastAPI()
 class AddFile(BaseModel):
     """Add a file."""
 
+    type: Literal["AddFile"]
     name: str
-    path: str
+    path: Path
     xs: list[float]
     ys: list[float]
 
@@ -28,3 +32,5 @@ async def websocket_endpoint(websocket: WebSocket) -> None:
     while True:
         raw_data = await websocket.receive_bytes()
         print(raw_data)
+        data = Message.model_validate_json(raw_data)
+        print(data)
--- frontend/src/views/analysis.rs
+++ frontend/src/views/analysis.rs
@@ -1,9 +1,29 @@
 use crate::components::Dropzone;
 use dioxus::prelude::*;
 use futures_util::{SinkExt, StreamExt};
-use std::{cell::RefCell, rc::Rc};
+use serde::{Deserialize, Serialize};
+use std::{cell::RefCell, path::PathBuf, rc::Rc};
 
 use tokio_tungstenite_wasm::connect;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+struct AddFile {
+    name: String,
+    path: PathBuf,
+    xs: Vec<f64>,
+    ys: Vec<f64>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(tag = "type")]
+enum Command {
+    AddFile(AddFile),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+struct Message {
+    command: Command,
+}
 
 #[component]
 pub fn Analysis() -> Element {
@@ -18,8 +38,16 @@ pub fn Analysis() -> Element {
                 tracing::info!("File changed");
                 spawn(async move {
                     if let Some(client) = &*client.read() {
-                        let msg = &b"hello"[..];
-                        client.borrow_mut().send(msg.into()).await.unwrap();
+                        let msg = Message {
+                            command: Command::AddFile(AddFile {
+                                name: "test".to_string(),
+                                path: PathBuf::from("/Users/lukas/projects/timal/add-websockets/backend/src/millie/main.py"),
+                                xs: vec![1.0, 2.0, 3.0],
+                                ys: vec![1.0, 2.0, 3.0],
+                            }),
+                        };
+                        let json = serde_json::to_vec(&msg).unwrap();
+                        client.borrow_mut().send(json.into()).await.unwrap();
                     }
                 });
             }
